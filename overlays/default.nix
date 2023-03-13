79af971691301a1b0869d1849b77cf8179b137ca
Chloe Bethel <chloe@9net.org>
2023-03-13 18:46:22 +0000

    Don't use panfork.

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,8 +4,6 @@
   ];
   perSystem = {config, ...}: {
     overlayAttrs = {
-      mesa = config.packages.mesa-panfork;
-      kodi-rock5b = config.packages.kodi;
       fan-control-rock5b = config.packages.fan-control;
       linux-rock5b = config.packages.linux-rock5b;
     };
