7e14a8352d59b26ccb5b13a1f294fddfcc975b05
Chloe Bethel <chloe@9net.org>
2024-01-16 19:38:37 +0000

    I don't control this

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
     };
 
   nixConfig = {
-    extra-substituters = ["https://rock5b-nixos.cachix.org"];
-    extra-trusted-public-keys = ["rock5b-nixos.cachix.org-1:bXHDewFS0d8pT90A+/YZan/3SjcyuPZ/QRgRSuhSPnA="];
+#    extra-substituters = ["https://rock5b-nixos.cachix.org"];
+#    extra-trusted-public-keys = ["rock5b-nixos.cachix.org-1:bXHDewFS0d8pT90A+/YZan/3SjcyuPZ/QRgRSuhSPnA="];
   };
 }
